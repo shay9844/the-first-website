aa0b5fe289c81b26a901f1807056591c61fc33da
Shay Mintzer <shaymintzer@gmail.com>
2019-12-04 09:33:27 +0200

    logo changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Instagram_logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,6 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
 		  <h2>this is awsome</h2>
         </p>
       </div>
