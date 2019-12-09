1b3c8e905f2dcc8401199aceb04a7c69bd345b06
Shay Mintzer <shaymintzer@gmail.com>
2019-12-09 08:54:27 +0200

    facebook and google logins

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import logo from './Instagram_logo.svg';
 import './App.css';
 
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
 class App extends Component {
+
   render() {
+
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
-      <div className="App">
+	
+	  <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Instagram market place</h2>
@@ -13,6 +28,26 @@ class App extends Component {
         <p className="App-intro">
 		  <h2>this is awsome</h2>
         </p>
+      </div>
+      <div className="App">
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+      <FacebookLogin
+        appId="" //APP ID NOT CREATED YET
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+      <br />
+      <br />
+
+
+      <GoogleLogin
+        clientId="" //CLIENTID NOT CREATED YET
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+
       </div>
     );
   }
